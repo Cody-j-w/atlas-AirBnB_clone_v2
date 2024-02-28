13fbc586f96459e8ee627957ee4c5d7098881f22
Cody Walenciak <codywalenciak@gmail.com>
2024-02-28 20:57:49 +0000

    Revert timestamp change

--- models/base_model.py
+++ models/base_model.py
@@ -23,15 +23,9 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            if 'updated_at' not in kwargs:
-                kwargs['updated_at'] = datetime.now()
-            else:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                   '%Y-%m-%dT%H:%M:%S.%f')
-            if 'created_at' not in kwargs:
-                kwargs['created_at'] = datetime.now()
-            else:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
