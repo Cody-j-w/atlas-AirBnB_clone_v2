5689a15a575010dc75f04787a795ac16a51b837f
Cody Walenciak <codywalenciak@gmail.com>
2024-03-08 20:22:21 +0000

    Add blank line

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,6 +27,7 @@ def c_route(text):
             new_str = new_str + i
     return 'C {}'.format(new_str)
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text="is cool"):
