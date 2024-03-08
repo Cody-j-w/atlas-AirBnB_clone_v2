8a8dfc24c866cfccf2f3188c99f9df893a25f56d
Cody <codywalenciak@gmail.com>
2024-03-08 10:59:59 -0600

    Update broken files with more functional versions

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
-from models.base_model import BaseModel
-from models.base_model import Base
-from models.place import place_amenity
-
-env = getenv('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
-    """Amenity model for HBNB"""
-
-    __tablename__ = 'amenities'
-    if env == 'db':
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
